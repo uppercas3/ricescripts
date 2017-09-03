32f7e4d1f132bca076e765da50c7d07533ce80e3
uppercas3 <potter.hrr@gmail.com>
2017-09-03 22:49:27 +0700

    Add aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+#wal 
+alias wao="wal -rt"
+alias woa="${HOME}/.config/bspwm/bspwmrc &"
+wao
+
+#feh 
+alias pape="feh --bg-fill"
+alias fehv="feh -.d"
+
+#git 
+alias gstt="git status"
+alias glog="git log"
+alias grr="git remote"
+alias grmr="git rm -r --cached"
+alias grm="git rm --cached"
